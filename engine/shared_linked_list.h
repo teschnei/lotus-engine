522607efd2be1c7bd5bd7fc6bb280bc03708574f
Tyler Schneider <teschnei@gmail.com>
2020-11-03 17:06:58 -0700

    Replaced atomic_ shared_ptr specialization functions with std::atomic<std::shared_ptr>

--- engine/shared_linked_list.h
+++ engine/shared_linked_list.h
@@ -14,11 +14,11 @@ namespace lotus
         SharedLinkedList& operator=(const SharedLinkedList<T>&) = delete;
         SharedLinkedList(SharedLinkedList<T>&& o)
         {
-            head = o.head;
+            head = o.head.exchange({}, std::memory_order::relaxed);
         }
         SharedLinkedList& operator=(SharedLinkedList<T>&& o)
         {
-            head = atomic_exchange_explicit(&o.head, {}, std::memory_order::seq_cst);
+            head = o.head.exchange({}, std::memory_order::seq_cst);
             return *this;
         }
 
@@ -26,26 +26,26 @@ namespace lotus
         {
             Node(T _val) : val(std::move(_val)) {}
             T val;
-            std::shared_ptr<Node> next;
+            std::atomic<std::shared_ptr<Node>> next;
         };
 
         std::optional<T> get()
         {
             if (head)
             {
-                return atomic_exchange_explicit(&head, head->next, std::memory_order::seq_cst);
+                return head.exchange(head->next, std::memory_order::seq_cst);
             }
             return {};
         }
 
         std::vector<T> getAll()
         {
-            auto node = atomic_exchange_explicit(&head, {}, std::memory_order::seq_cst);
+            auto node = head.exchange({}, std::memory_order::seq_cst);
             std::vector<T> values;
             while(node)
             {
                 values.push_back(std::move(node->val));
-                node = node->next;
+                node = node->next.load(std::memory_order::relaxed);
             }
             return values;
         }
@@ -56,12 +56,12 @@ namespace lotus
             std::shared_ptr<Node> expected_head;
             do
             {
-                expected_head = atomic_load_explicit(&head, std::memory_order::seq_cst);
-                node->next = expected_head;
-            } while (!atomic_compare_exchange_weak_explicit(&head, &expected_head, node, std::memory_order::seq_cst, std::memory_order::relaxed));
+                expected_head = head.load(std::memory_order::seq_cst);
+                node->next.store(expected_head, std::memory_order::relaxed);
+            } while (!head.compare_exchange_weak(expected_head, node, std::memory_order::seq_cst, std::memory_order::relaxed));
         }
     private:
-        std::shared_ptr<Node> head;
+        std::atomic<std::shared_ptr<Node>> head;
     };
 }
 
